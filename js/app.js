a9f43ebcb45690adbd7570316e249810a00398b4
zjaredkhan <jared.khan@outlook.com>
2018-10-20 15:52:09 -0700

    actually saved stuff that was missed before

--- js/app.js
+++ js/app.js
@@ -8,15 +8,18 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  new CartItem(this.product,this.quantity).push(Product.allProducts);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('savedCart', JSON.stringify(Product.allProducts));
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function(indexNumber) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  Product.allProducts.splice(indexNumber);
 };
 
 var CartItem = function(product, quantity) {
